401cb772e00b71e722c586270e51b1325c2dc660
legoChoi <chlx159@gmail.com>
2025-05-03 02:23:18 +0900

    init

--- src/main/java/test/msa/user/UserServiceApplication.java
+++ src/main/java/test/msa/user/UserServiceApplication.java
@@ -2,7 +2,9 @@ package test.msa.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserServiceApplication {
 
